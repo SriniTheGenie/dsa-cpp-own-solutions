6e4418f15d91552850d205c2f256cb3f02592133
SriniTheGenie <98487597+SriniTheGenie@users.noreply.github.com>
2022-05-20 14:00:09 +0530

    Update LargestElement.cpp

--- Arrays/1/LargestElement.cpp
+++ Arrays/1/LargestElement.cpp
@@ -1,3 +1,24 @@
+/* for ( range_declaration : range_expression ) 
+    loop_statement
+
+Parameters :
+range_declaration : 
+a declaration of a named variable, whose type is the 
+type of the element of the sequence represented by 
+range_expression, or a reference to that type.
+Often uses the auto specifier for automatic type 
+deduction.
+
+range_expression : 
+any expression that represents a suitable sequence 
+or a braced-init-list.
+
+loop_statement : 
+any statement, typically a compound statement, which
+is the body of the loop. */
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
