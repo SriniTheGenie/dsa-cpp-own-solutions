c39f38eee191efcbd880a39b6ee4d0c9c4541ca1
SriniTheGenie <98487597+SriniTheGenie@users.noreply.github.com>
2022-05-22 23:53:02 +0530

    Update Optimized Bubble Sort.cpp

--- Basic Sorting Algorithms/1/Optimized Bubble Sort.cpp
+++ Basic Sorting Algorithms/1/Optimized Bubble Sort.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 vector<int> optimizedBubbleSort(vector<int> v){
     // your code  goes here
-    int sorted = 1;
+    bool sorted;
     int n=v.size();
      for(int times=1;times<=n-1;times++)
      {
+         sorted = true;
          for(int j=0;j<=n-1-times;j++)
          {
              if(v[j]>v[j+1])
              {
                  swap(v[j],v[j+1]);
-                 sorted = 0;
+                 sorted = false;
              }
          }
          if(sorted)
