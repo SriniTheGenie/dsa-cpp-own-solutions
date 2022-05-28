a5bc9a1af513f66e7b0346574d28645d9401490d
SriniTheGenie <98487597+SriniTheGenie@users.noreply.github.com>
2022-05-28 21:39:47 +0530

    Update Chopsticks.cpp

--- Basic Sorting Algorithms/4/Chopsticks.cpp
+++ Basic Sorting Algorithms/4/Chopsticks.cpp
@@ -1,6 +1,7 @@
 /* Why do we need sorting here?
    Only if we do sorting, if the difference between the nearby elements is !(<=D) we shall not process the element further.
    Whereas if sorting is not done, we will endup questioning an element again and again which is not a correct solution */
+//Expected Complexity: O(N logN)
 
 #include<bits/stdc++.h>
 using namespace std;
