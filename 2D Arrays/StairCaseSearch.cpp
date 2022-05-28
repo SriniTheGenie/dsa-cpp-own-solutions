90353eddec0e3cbcba1b4da6aba5c5d4cece4a0a
SriniTheGenie <98487597+SriniTheGenie@users.noreply.github.com>
2022-05-28 22:05:37 +0530

    Update StairCaseSearch.cpp

--- 2D Arrays/StairCaseSearch.cpp
+++ 2D Arrays/StairCaseSearch.cpp
@@ -1,1 +1,5 @@
 //for staircase search algorithm refer to Sudarsan Rajagopalan's repository
+
+
+// In the worst case we are traversing One row and One column in staircase search, so we have effectively reduced the
+// complexity from quadratic O(N^2) to O(N+M) <=> O(row + column)
